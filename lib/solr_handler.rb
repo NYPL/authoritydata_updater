baf1dcb78f0adc70cedfd5afc4053556f11c3493
nodanaonlyzuul <beholdthepanda@gmail.com>
2019-08-28 11:40:33 -0400

    Delete by query before posting

--- lib/solr_handler.rb
+++ lib/solr_handler.rb
@@ -21,10 +21,25 @@ class RSolr::Connection
 end
 
 class SolrHandler
+  @@logger = NyplLogFormatter.new(STDOUT, level: 'debug')
+
   def self.send_docs_to_solr(solr_url, docs)
     solr = RSolr.connect(:url => solr_url)
-    # Post to Solr in batches of 1,000
-    docs.each_slice(1000) do |batch|
+
+    authority_code = docs.first[:authority_code]
+    if authority_code
+      delete_query = "authority_code:#{authority_code}"
+      @@logger.debug("deleting by query: #{delete_query}")
+      response = solr.delete_by_query(delete_query)
+      @@logger.info("delete response: #{response.to_s}")
+      response = solr.commit
+      @@logger.info("commit response: #{response.to_s}")
+    else
+      @@logger.info("Skipping delete_by_query because I coudn't find an authority_code '#{authority_code}'")
+    end
+
+    # Post to Solr in batches
+    docs.each_slice(2000) do |batch|
       solr.add(batch.compact)
       solr.commit
     end
