dfe363e5ffe7fdbbf0d13b55c25a01caaaed8dc9
nodanaonlyzuul <beholdthepanda@gmail.com>
2019-09-03 16:05:19 -0400

    Use redis, not a hash in memory

--- lib/tripple_to_solr_doc.rb
+++ lib/tripple_to_solr_doc.rb
@@ -3,16 +3,19 @@
 require 'redis'
 require 'rdf'
 require 'linkeddata'
+require 'redis'
 include RDF
 
 class TrippleToSolrDoc
   @@logger = NyplLogFormatter.new(STDOUT, level: 'debug')
+  @@redis = Redis.new(url: ENV['REDIS_URL'])
 
   def self.convert!(file:, term_type:, authority_code:, authority_name:, unique_id_prefix:)
+    # remove all_subjects
     all_subjects = {}
     statement_count = 0
 
-    File.open(file,'r').each do |line|
+    File.open(file, 'r').each do |line|
       # Almost all predicates show up once per Subject, but a subject can have
       # multiple "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" predicates
       RDF::NTriples::Reader.new(line) do |reader|
@@ -22,35 +25,38 @@ class TrippleToSolrDoc
           @@logger.debug("parsing statement # #{statement_count}")
           subject_url = statement.subject.to_s
 
-          if all_subjects[subject_url]
-            # We've seen this subject before
-            if predicate_string == 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
-              if all_subjects[subject_url][predicate_string]
-                all_subjects[subject_url][predicate_string] << statement.object.to_s
+          if @@redis.exists(subject_url)
+            # We've seen this subject before...
+            this_subjects_attributes = Marshal.load(@@redis.get(subject_url))
+
+             if predicate_string == 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+              if this_subjects_attributes[predicate_string]
+                this_subjects_attributes[predicate_string] << statement.object.to_s
               else
                 # This is the first time we've seen syntax-ns#type
-                all_subjects[subject_url].merge!(predicate_string => [statement.object.to_s])
+                this_subjects_attributes.merge!(predicate_string => [statement.object.to_s])
               end
             else
-              all_subjects[subject_url].merge!(predicate_string => statement.object.to_s)
+              this_subjects_attributes.merge!(predicate_string => statement.object.to_s)
             end
+
+            @redis.set(subject_url, Marshal.dump(this_subjects_attributes))
           else
-            # This is the first time we've seen this subject
+            # We've never seen this subject before
+
             if predicate_string == 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
-              all_subjects[subject_url] = {
-                predicate_string => [statement.object.to_s]
-              }
+              inital_hash = Marshal.dump({predicate_string => [statement.object.to_s]})
+              @redis.set(subject_url, inital_hash)
             else
-              all_subjects[subject_url] = {
-                predicate_string => statement.object.to_s
-              }
+              initial_hash = Marshal.dump({predicate_string => statement.object.to_s})
+              @redis.set(subject_url, initial_hash)
             end
           end
         end
       end
     end
 
-    @@logger.info("before weeding out there were #{all_subjects.keys.length}")
+    @@logger.info("before weeding out there were #{@@redis.dbsize}")
 
     # Weed out out old /deprecated subjects,
     # changeNote predicates point to a bnode lines.
@@ -58,25 +64,39 @@ class TrippleToSolrDoc
     #  <http://id.loc.gov/vocabulary/graphicMaterials/tgm003368> <http://www.loc.gov/mads/rdf/v1#adminMetadata> _:bnode12683670136320746870
     #  ...and looking at _:bnode12683670136320746870
     #  :bnode12683670136320746870 <http://id.loc.gov/ontologies/RecordInfo#recordStatus'> "deprecated"
-    all_subjects.reject! do |_subject, attributes|
+    @@redis.scan_each do |subject, attributes_as_string|
+      attributes = Marshal.load(attributes_as_string)
       change_history = attributes['http://www.loc.gov/mads/rdf/v1#adminMetadata']
-      all_subjects.dig(change_history, 'http://id.loc.gov/ontologies/RecordInfo#recordStatus') == 'deprecated'
+      bnode_for_this = @@redis.get(change_history)
+
+      if change_history && bnode_for_this
+        bnodes_hash = Marshal.load(bnode_for_this)
+        if bnodes_hash['http://id.loc.gov/ontologies/RecordInfo#recordStatus'] == 'deprecated'
+          @@redis.del(subject)
+        end
+      end
     end
 
-    @@logger.info("after weeding out deprecated there were #{all_subjects.keys.length}")
+    @@logger.info("after weeding out deprecated there were #{@@redis.dbsize}")
 
     # Weed out bnodes, you have to do this after weeding out deprecateds, not at the same time
-    all_subjects.select! do |subject, _attributes|
-      subject.include?('http')
+    @@redis.scan_each do |subject, attributes_as_string|
+      unless subject.include?('http')
+        @@redis.del(subject)
+      end
     end
-    @@logger.info("after weeding out bnodes there were #{all_subjects.keys.length}")
 
-    solr_docs = all_subjects.map do |subject, attributes|
-      {
+    @@logger.info("after weeding out bnodes there were #{@@redis.dbsize}")
+
+    solr_docs = []
+    @@redis.scan_each do |subject, attr|
+      attributes = Marshal.load(attr)
+
+      new_document = {
         uri: subject,
         term: attributes.dig('http://www.loc.gov/mads/rdf/v1#authoritativeLabel'),
         term_idx: attributes.dig('http://www.loc.gov/mads/rdf/v1#authoritativeLabel'),
-        term_type: (term_type == :auto) ? detect_term_type(attributes) : term_type,
+        term_type: term_type == :auto ? detect_term_type(attributes) : term_type,
         record_id: File.basename(subject),
         language: 'en',
         authority_code: authority_code,
@@ -102,10 +122,10 @@ class TrippleToSolrDoc
       'http://www.loc.gov/mads/rdf/v1#Title' => 'title',
       'http://www.loc.gov/mads/rdf/v1#ConferenceName' => 'name_conference'
     }
-    
-    terms = term_types.keys    
+
+    terms = term_types.keys
     ns_types = predicate_to_object_mapping.dig('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
-    
+
     term_types[(terms & ns_types)&.first]
   end
 end
