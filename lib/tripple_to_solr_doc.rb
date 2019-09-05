7fa6d73cb80431e34679a7629dc861a10147e44c
nodanaonlyzuul <beholdthepanda@gmail.com>
2019-09-05 10:22:39 -0400

    Factoring out static variables to class constant.

--- lib/tripple_to_solr_doc.rb
+++ lib/tripple_to_solr_doc.rb
@@ -7,11 +7,31 @@ require 'redis'
 include RDF
 
 class TrippleToSolrDoc
+  NS_TYPE_TO_TERM_TYPE_MAPPING = {
+    'http://www.loc.gov/mads/rdf/v1#Topic' => 'topic',
+    'http://www.loc.gov/mads/rdf/v1#Geographic' => 'geographic',
+    'http://www.loc.gov/mads/rdf/v1#PersonalName' => 'name_personal',
+    'http://www.loc.gov/mads/rdf/v1#ComplexSubject' => 'complex_subject',
+    'http://www.loc.gov/mads/rdf/v1#CorporateName' => 'name_corporate',
+    'http://www.loc.gov/mads/rdf/v1#GenreForm' => 'genreform',
+    'http://www.loc.gov/mads/rdf/v1#Temporal' => 'temporal',
+    'http://www.loc.gov/mads/rdf/v1#NameTitle' => 'name_title',
+    'http://www.loc.gov/mads/rdf/v1#Title' => 'title',
+    'http://www.loc.gov/mads/rdf/v1#ConferenceName' => 'name_conference'
+  }.freeze
+
+  WORTHWHILE_PREDICATES = [
+    'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+    'http://www.loc.gov/mads/rdf/v1#adminMetadata',
+    'http://id.loc.gov/ontologies/RecordInfo#recordStatus',
+    'http://www.loc.gov/mads/rdf/v1#authoritativeLabel',
+    'http://www.w3.org/2004/02/skos/core#prefLabel'
+  ].freeze
+
   @@logger = NyplLogFormatter.new(STDOUT, level: 'debug')
   @@redis = Redis.new(url: ENV['REDIS_URL'])
 
   def self.convert!(file:, term_type:, authority_code:, authority_name:, unique_id_prefix:)
-
     statement_count = 0
 
     File.open(file, 'r').each do |line|
@@ -44,15 +64,15 @@ class TrippleToSolrDoc
               # We've never seen this subject before
 
               if predicate_string == 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
-                inital_hash = Marshal.dump({predicate_string => [statement.object.to_s]})
+                inital_hash = Marshal.dump(predicate_string => [statement.object.to_s])
                 @@redis.set(subject_url, inital_hash)
               else
-                initial_hash = Marshal.dump({predicate_string => statement.object.to_s})
+                initial_hash = Marshal.dump(predicate_string => statement.object.to_s)
                 @@redis.set(subject_url, initial_hash)
               end
             end
           else
-            @@logger.info("skipping a statement")
+            @@logger.info('skipping a statement')
           end
         end
       end
@@ -67,49 +87,47 @@ class TrippleToSolrDoc
     #  ...and looking at _:bnode12683670136320746870
     #  :bnode12683670136320746870 <http://id.loc.gov/ontologies/RecordInfo#recordStatus'> "deprecated"
     @@redis.scan_each do |subject|
-        attributes = Marshal.load(@@redis.get(subject))
-        change_history = attributes['http://www.loc.gov/mads/rdf/v1#adminMetadata']
-        bnode_for_this = @@redis.get(change_history)
+      attributes = Marshal.load(@@redis.get(subject))
+      change_history = attributes['http://www.loc.gov/mads/rdf/v1#adminMetadata']
+      bnode_for_this = @@redis.get(change_history)
 
-        if change_history && bnode_for_this
-          bnodes_hash = Marshal.load(bnode_for_this)
-          if bnodes_hash['http://id.loc.gov/ontologies/RecordInfo#recordStatus'] == 'deprecated'
-            @@redis.del(subject)
-          end
+      if change_history && bnode_for_this
+        bnodes_hash = Marshal.load(bnode_for_this)
+        if bnodes_hash['http://id.loc.gov/ontologies/RecordInfo#recordStatus'] == 'deprecated'
+          @@redis.del(subject)
         end
+      end
     end
 
     @@logger.info("after weeding out deprecated there were #{@@redis.dbsize}")
 
     # Weed out bnodes, you have to do this after weeding out deprecateds, not at the same time
     @@redis.scan_each do |subject|
-      unless subject.include?('http')
-        @@redis.del(subject)
-      end
+      @@redis.del(subject) unless subject.include?('http')
     end
 
     @@logger.info("after weeding out bnodes there were #{@@redis.dbsize}")
 
     solr_docs = []
     @@redis.scan_each do |subject|
-        attributes = Marshal.load(@@redis.get(subject))
+      attributes = Marshal.load(@@redis.get(subject))
 
-        new_document = {
-          uri: subject,
-          term: attributes.dig('http://www.loc.gov/mads/rdf/v1#authoritativeLabel'),
-          term_idx: attributes.dig('http://www.loc.gov/mads/rdf/v1#authoritativeLabel'),
-          term_type: term_type == :auto ? detect_term_type(attributes) : term_type,
-          record_id: File.basename(subject),
-          language: 'en',
-          authority_code: authority_code,
-          authority_name: authority_name,
-          unique_id: "#{unique_id_prefix}_#{File.basename(subject)}",
-          alternate_term_idx: attributes.dig('http://www.w3.org/2004/02/skos/core#prefLabel'),
-          alternate_term: attributes.dig('http://www.w3.org/2004/02/skos/core#prefLabel')
-        }
+      new_document = {
+        uri: subject,
+        term: attributes.dig('http://www.loc.gov/mads/rdf/v1#authoritativeLabel'),
+        term_idx: attributes.dig('http://www.loc.gov/mads/rdf/v1#authoritativeLabel'),
+        term_type: term_type == :auto ? detect_term_type(attributes) : term_type,
+        record_id: File.basename(subject),
+        language: 'en',
+        authority_code: authority_code,
+        authority_name: authority_name,
+        unique_id: "#{unique_id_prefix}_#{File.basename(subject)}",
+        alternate_term_idx: attributes.dig('http://www.w3.org/2004/02/skos/core#prefLabel'),
+        alternate_term: attributes.dig('http://www.w3.org/2004/02/skos/core#prefLabel')
+      }
 
-        solr_docs << new_document
-      end
+      solr_docs << new_document
+    end
     solr_docs
   end
 
@@ -118,34 +136,11 @@ class TrippleToSolrDoc
   # As we parse the N-tripple file, we can skip even considering
   # this line if it's dealing with a predicate we don't care about (e.g. http://www.w3.org/2000/01/rdf-schema#seeAlso)
   def self.worthwhile_statement?(statement)
-    # TODO: worthehile_predicates can be factored out into a class constant
-    worthwhile_predicates = [
-      'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
-      'http://www.loc.gov/mads/rdf/v1#adminMetadata',
-      'http://id.loc.gov/ontologies/RecordInfo#recordStatus',
-      'http://www.loc.gov/mads/rdf/v1#authoritativeLabel',
-      "http://www.w3.org/2004/02/skos/core#prefLabel"
-    ]
-
-    worthwhile_predicates.include?(statement.predicate.to_s)
+    WORTHWHILE_PREDICATES.include?(statement.predicate.to_s)
   end
 
   def self.detect_term_type(predicate_to_object_mapping)
-    # TODO: term_types can be factored out into a class constant
-    term_types = {
-      'http://www.loc.gov/mads/rdf/v1#Topic' => 'topic',
-      'http://www.loc.gov/mads/rdf/v1#Geographic' => 'geographic',
-      'http://www.loc.gov/mads/rdf/v1#PersonalName' => 'name_personal',
-      'http://www.loc.gov/mads/rdf/v1#ComplexSubject' => 'complex_subject',
-      'http://www.loc.gov/mads/rdf/v1#CorporateName' => 'name_corporate',
-      'http://www.loc.gov/mads/rdf/v1#GenreForm' => 'genreform',
-      'http://www.loc.gov/mads/rdf/v1#Temporal' => 'temporal',
-      'http://www.loc.gov/mads/rdf/v1#NameTitle' => 'name_title',
-      'http://www.loc.gov/mads/rdf/v1#Title' => 'title',
-      'http://www.loc.gov/mads/rdf/v1#ConferenceName' => 'name_conference'
-    }
-
-    terms = term_types.keys
+    terms = NS_TYPE_TO_TERM_TYPE_MAPPING.keys
     ns_types = predicate_to_object_mapping.dig('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
 
     term_types[(terms & ns_types)&.first]
