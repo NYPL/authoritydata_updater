fc37d5d9815e0b896d1b475ea4427d9ef1c6c355
Chris Baker <consultcbaker@nypl.org>
2021-02-11 09:50:23 -0800

    remove unused spec dir

--- spec/spec_helper.rb
+++
@@ -1,5 +0,0 @@
-# spec/spec_helper.rb
-require 'rack/test'
-require 'rspec'
-
-ENV['RACK_ENV'] = 'test'
--- spec/vocabulary_parser_spec.rb
+++
@@ -1,7 +0,0 @@
-# spec/vocabulary_parser_spec.rb
-require_relative 'spec_helper.rb'
-require_relative '../lib/vocabulary_parser.rb'
-
-describe "vocabulary parser" do
-    pending 'ouputs a JSON file'
-end
