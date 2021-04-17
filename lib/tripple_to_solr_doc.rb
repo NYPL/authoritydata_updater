3910ccf94e9ff2f61d9d2f28f8ae883d6bd1ceb5
Chris Baker <consultcbaker@nypl.org>
2021-04-17 10:19:09 -0700

    more frequent compacting

--- lib/tripple_to_solr_doc.rb
+++ lib/tripple_to_solr_doc.rb
@@ -9,7 +9,7 @@ include RDF
 class TrippleToSolrDoc
   # The intermediate, rdbm file can get GIANT with names
   # This strips deprecated subjects every Nth iteration
-  COMPACT_EVERY = 1000000
+  COMPACT_EVERY = 10000
 
   # Almost all predicates show up once per Subject, but a subject can have
   # multiple predicates e.g "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
