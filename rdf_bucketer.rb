be3fc8d4ed164c44cffef019daa36b3d83ba9e9a
Chris Baker <consultcbaker@nypl.org>
2021-05-03 13:17:49 -0700

    bucketer without rdf

--- rdf_bucketer.rb
+++ rdf_bucketer.rb
@@ -2,11 +2,15 @@
 
 require "digest"
 require "optparse"
-require "rdf"
-include RDF
+require "progress_bar"
 
-RDF_SUBJECT_PATTERN = /(.+?)\s/
-BATCH_SIZE = 10000
+REGEX_RDF_TRIPPLES = /^(?<subject>.+?) <(?<predicate>.+?)> (?<object>.+?) \.$/  # matches each line in an RDF tripples file
+REGEX_LITERAL_WITH_LANGUAGE = /^\"(?<value>.+)\"@(?<language>.\w+)$/            # e.g. "Abstract films"@en
+REGEX_LITERAL = /^\"?(?<value>.+?)\"?$/                                         # literal without a language tag
+REGEX_IRI = /^<(?<value>.+)>$/                                                  # e.g. <http://id.loc.gov/authorities/genreForms/gf2011026043>
+REGEX_NOT_BLANK = /[^[:space:]]/                                                # equivalent to !ActiveSupport.blank?
+
+READ_FILE_BATCH_SIZE = 10000
 
 options = {}
 opt_parser = OptionParser.new do |opts|
@@ -37,23 +41,22 @@ output_files = 1.upto(options[:buckets]).map do |n|
   File.open("#{options[:source]}.#{n}", "w")
 end
 
-statements = 0
-File.open(options[:source], "r") do |input_file|
-  input_file.lazy.each_slice(BATCH_SIZE) do |lines|
-    statements += lines.size
-    puts "Processing line #{statements}..."
+puts "Source: #{options[:source]}"
+print "\tcounting lines... "
+total_lines = %x{wc -l #{options[:source]}}.split.first.to_i
+puts total_lines
+bar = ProgressBar.new(total_lines)
 
-    lines.each do |line|
-      match = line.match(RDF_SUBJECT_PATTERN)
-      next unless match && match[1]
-      subject = match[1]
-
-      # determine the bucket for this subject by taking a MD5 hash of the subject URI,
-      # converting that to an integer, modulo the number of buckets
-      bucket = Digest::MD5.hexdigest(subject).to_i(16) % options[:buckets]
-
-      output_files[bucket].write(line)
+File.open(options[:source], "r") do |file|
+  file.lazy.each_slice(READ_FILE_BATCH_SIZE) do |batch|
+    batch.each do |line|
+      if matches = line.match(REGEX_RDF_TRIPPLES)
+        bucket = Digest::MD5.hexdigest(matches[:subject]).to_i(16) % options[:buckets]
+        output_files[bucket].write(line)
+      end
     end
+
+    bar.increment!(batch.size)
   end
 end
 
