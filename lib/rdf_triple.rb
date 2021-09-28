a0d20e1f4bd59f1208925fa16a5d42a759a48656
Chris Baker <consultcbaker@nypl.org>
2021-09-28 10:05:35 -0700

    RdfTriple class with parsing

---
+++ lib/rdf_triple.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+REGEX_RDF_TRIPPLES = /^(?<subject>.+?) <(?<predicate>.+?)> (?<object>.+?) \.$/
+
+class RdfTriple
+  attr_reader :subject, :predicate, :object
+
+  def self.parse(triple_string)
+    matches = triple_string.match(REGEX_RDF_TRIPPLES)
+    raise ArgumentError unless matches
+
+    new matches[:subject], matches[:predicate], matches[:object]
+  end
+
+  def initialize(subject, predicate, object)
+    @subject = subject
+    @predicate = predicate
+    @object = object
+  end
+end
---
+++ test/test_rdf_triple.rb
@@ -0,0 +1,24 @@
+require "rdf_triple"
+require "pry"
+
+class TestRdfTriple < Test::Unit::TestCase
+  RDF_TEST_SUBJECT = "<http://id.loc.gov/authorities/genreForms/gf2011026028>"
+  RDF_TEST_PREDICATE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+  RDF_TEST_OBJECT = "<http://www.loc.gov/mads/rdf/v1#GenreForm>"
+
+  RDF_TRIPLE_LINE = "#{RDF_TEST_SUBJECT} <#{RDF_TEST_PREDICATE}> #{RDF_TEST_OBJECT} ."
+
+  def test_parsing
+    triple = RdfTriple.parse(RDF_TRIPLE_LINE)
+
+    assert_equal triple.subject, RDF_TEST_SUBJECT
+    assert_equal triple.predicate, RDF_TEST_PREDICATE
+    assert_equal triple.object, RDF_TEST_OBJECT
+  end
+
+  def test_parse_invalid
+    assert_raises ArgumentError do
+      RdfTriple.parse("this is an invalid rdf line")
+    end
+  end
+end
